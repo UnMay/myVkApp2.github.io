829447f341bee4d65dec7da39ff109476c26ad02
UnMay <necromant650@ya.ru>
2019-05-28 13:51:26 +0500

    Updates

---
+++ precache-manifest.7d24b9d47f77dc8fca59ba0be56975cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "603e5a52ae81787e4408",
+    "url": "/static/js/main.603e5a52.chunk.js"
+  },
+  {
+    "revision": "3da503221350140819d4",
+    "url": "/static/js/2.3da50322.chunk.js"
+  },
+  {
+    "revision": "603e5a52ae81787e4408",
+    "url": "/static/css/main.de0b5d3f.chunk.css"
+  },
+  {
+    "revision": "3da503221350140819d4",
+    "url": "/static/css/2.1291a2f0.chunk.css"
+  },
+  {
+    "revision": "14145fc2ff14be556b8492663230b16a",
+    "url": "/index.html"
+  }
+];
